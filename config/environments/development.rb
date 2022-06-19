d69f34e6db3b2b482ea9bc6e8d66d507180ed537
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-19 12:39:22 -0500

    Fix pictures

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.logger = Logger.new($stdout)
+  # config.logger = Logger.new($stdout)
   # Show full error reports.
   config.consider_all_requests_local = true
 
