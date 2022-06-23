4cef8655d360de5db336bfd4706f0c2e206b7c5b
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-23 12:24:14 -0500

    Add cover

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,7 +1,7 @@
 class EmployeeSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :country, :region, :document_id, :document_type, :contact, :created_at,
              :experience, :user_type, :email, :token, :image_url, :new,
-             :user_id, :phone, :biografy, :lada
+             :user_id, :phone, :biografy, :lada, :cover
 
   def user_type
     object.user.user_type
