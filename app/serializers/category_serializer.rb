cd8a8cd35649f88f38f2688da788026a7711f7cc
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-24 12:45:06 -0500

    Add image to category

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,6 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :body, :category_name, :price_col_complete, :price_col_half, :price_spain, :image_url, :region, :sector
+  attributes :id, :body, :category_name, :price_col_complete, :price_col_half, :price_spain,
+             :image_url, :region, :sector, :image
 
   def sector
     object.sector.name
