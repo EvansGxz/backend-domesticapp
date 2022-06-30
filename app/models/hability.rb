a51fa980b88e97f224d976ef7b8eaafb72109383
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-30 02:06:09 -0500

    Add

--- app/models/hability.rb
+++ app/models/hability.rb
@@ -2,7 +2,6 @@ class Hability < ApplicationRecord
   has_one_attached :image
   has_one :hability_employee, dependent: :destroy
 
-
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
   end
