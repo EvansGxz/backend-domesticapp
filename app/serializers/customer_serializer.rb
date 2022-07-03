41ece759d973107859c02fb2d10f79b0a6e85847
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-03 11:31:52 -0500

    Add cover in customer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,6 +1,6 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :email, :phone, :document_id, :client_type, :birth_date, :region,
-             :cod_refer, :country, :document_type, :user_type, :full_name,
+             :cod_refer, :country, :document_type, :user_type, :full_name, :cover,
              :token, :new, :image_url, :social_id, :encargado, :user_id, :lada, :created_at
   def user_type
     object.user.user_type
