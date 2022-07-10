147effbe0ed3d98f7f6ae11de2cb3c21915f7268
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-10 16:04:13 -0500

    Add customer id

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,12 +1,8 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :photo, :employee_id, :body, :score
+  attributes :id, :full_name, :photo, :employee_id, :body, :score, :customer_id
 
   def full_name
     object.customer.full_name
-  end
-
-  def employee_id
-    object.employee.id
   end
 
   def photo
