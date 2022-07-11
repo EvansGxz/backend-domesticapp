7582cbd8cdaa26f309dcab38c1788cb03adf5243
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-10 21:54:12 -0500

    Add image url

--- app/models/service_report.rb
+++ app/models/service_report.rb
@@ -1,4 +1,8 @@
 class ServiceReport < ApplicationRecord
   belongs_to :order_dtail
   has_one_attached :cover
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(cover) if cover.attached?
+  end
 end
--- app/serializers/service_report_serializer.rb
+++ app/serializers/service_report_serializer.rb
@@ -1,5 +1,5 @@
 class ServiceReportSerializer < ActiveModel::Serializer
-  attributes :id, :body, :order_detail_id, :employee_id, :employee_name
+  attributes :id, :body, :order_detail_id, :employee_id, :employee_name, :image_url
 
   def employee_name
     object.employee.full_name
