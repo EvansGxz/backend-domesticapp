859db9d54ad98b48342eca2c5835ba77683c31c5
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-11 17:32:36 -0500

    Add birth date

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,7 +1,7 @@
 class EmployeeSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :country, :region, :document_id, :document_type, :contact, :created_at,
              :experience, :user_type, :email, :token, :image_url, :new, :contrato,
-             :user_id, :phone, :biografy, :lada, :cover, :contrato_url
+             :user_id, :phone, :biografy, :lada, :cover, :contrato_url, :birth_date
 
   def user_type
     object.user.user_type
